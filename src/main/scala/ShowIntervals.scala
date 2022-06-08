244f52733b0a42027d166165619852f6f57491f9
FilipeSilva-Adentis <filipe.silva@adentis.pt>
2022-06-08 14:04:56 +0100

    Dinamic implemented but not correct

--- src/main/scala/ShowIntervals.scala
+++ src/main/scala/ShowIntervals.scala
@@ -8,11 +8,15 @@ object ShowIntervals {
 
   sealed trait MonthsSinceNow
 
+  def totalMonths(date: YearMonth, initalDate: YearMonth): Int = {
+    val month = ((date.getYear.intValue * 12 + date.getMonth.getValue) - (initalDate.getYear.intValue() * 12 + initalDate.getMonth.getValue))
+    //        println("Order: " + date + " - IniDate: " + initalDate + " Diference: " + month)
+    month
+  }
 
   def getIntervalsWithIntervals(initalDate: YearMonth, endDate: YearMonth, intervalsArg: String, list: Seq[Order]): String = {
 
     val listOrders = getOrders(initalDate, endDate, list)
-
 
     val matcher = Pattern.compile("\\d+").matcher(intervalsArg)
 
@@ -21,58 +25,28 @@ object ShowIntervals {
       matcher.find
     }) intervals.add(Integer.valueOf(matcher.group))
 
-    for (w <- 0 until intervals.size() - 1) {
+    //    println(intervals)
+    var str: String = new String()
+
+    for (w <- 0 until intervals.size()) {
       if (w % 2 == 0) {
-        val aux = intervals.get(w.toString.toInt).toString + "-" + w
-        case object w.toString extends MonthsSinceNow
-      }
-    }
+        var counter: Int = 0
+        if (w + 1 != intervals.size()) {
+          listOrders.foreach(order => {
+            val space = totalMonths(order.date, initalDate)
 
-    case object LessThanOneMonth extends MonthsSinceNow
-    case object OneToThreeMonths extends MonthsSinceNow
-    case object FourToSixMonths extends MonthsSinceNow
-    case object SevenToTwelveMonths extends MonthsSinceNow
-    case object MoreThanTwelveMonths extends MonthsSinceNow
-
-    object MonthsSinceNow {
-      def totalMonths(date: YearMonth): Int = {
-        val month = ((date.getYear.intValue * 12 + date.getMonth.getValue) - (initalDate.getYear.intValue() * 12 + initalDate.getMonth.getValue))
-        //        println("Order: " + date + " - IniDate: " + initalDate + " Diference: " + month)
-        month
-      }
-
-      def apply(order: Order): MonthsSinceNow = {
-        val delta = totalMonths(order.date)
-        delta match {
-          case delta if delta < 1 => LessThanOneMonth
-          case delta if delta == 1 || delta <= 3 => OneToThreeMonths
-          case delta if delta == 4 || delta <= 6 => FourToSixMonths
-          case delta if delta == 7 || delta <= 12 => SevenToTwelveMonths
-          case delta if delta > 12 => MoreThanTwelveMonths
+            if (space >= intervals.get(w) && space <= intervals.get(w + 1)) {
+              counter += 1
+            }
+          })
+          str += s"${intervals.get(w)}-${intervals.get(w + 1)}: ${counter} \n"
         }
       }
     }
 
-    val orders: Seq[Order] = listOrders
-
-    val a: Seq[MonthsSinceNow] =
-      orders.map(order => MonthsSinceNow.apply(order))
-
-    val b: Map[MonthsSinceNow, Seq[MonthsSinceNow]] = a.groupBy(identity)
-
-    val c: Map[MonthsSinceNow, Int] = b.map { case (monthsSinceNow, orders) =>
-      (monthsSinceNow, orders.size)
-    }
-
-    c.map(obj => {
-      obj._1 match {
-        case LessThanOneMonth => ("<1: " + obj._2)
-        case OneToThreeMonths => ("1-3: " + obj._2)
-        case FourToSixMonths => ("4-6: " + obj._2)
-        case SevenToTwelveMonths => ("7-12: " + obj._2)
-        case MoreThanTwelveMonths => (">12: " + obj._2)
-      }
-    }).mkString("\n")
+//    println("-" + str + "\n")
+    //    println(str)
+    str.toString
   }
 
   def getIntervals(initalDate: YearMonth, endDate: YearMonth, list: Seq[Order]): String = {
@@ -86,14 +60,8 @@ object ShowIntervals {
     case object MoreThanTwelveMonths extends MonthsSinceNow
 
     object MonthsSinceNow {
-      def totalMonths(date: YearMonth): Int = {
-        val month = ((date.getYear.intValue * 12 + date.getMonth.getValue) - (initalDate.getYear.intValue() * 12 + initalDate.getMonth.getValue))
-        //        println("Order: " + date + " - IniDate: " + initalDate + " Diference: " + month)
-        month
-      }
-
       def apply(order: Order): MonthsSinceNow = {
-        val delta = totalMonths(order.date)
+        val delta = totalMonths(order.date, initalDate)
         delta match {
           case delta if delta < 1 => LessThanOneMonth
           case delta if delta == 1 || delta <= 3 => OneToThreeMonths
@@ -111,13 +79,14 @@ object ShowIntervals {
 
     val b: Map[MonthsSinceNow, Seq[MonthsSinceNow]] = a.groupBy(identity)
 
-    val c: Map[MonthsSinceNow, Int] = b.map { case (monthsSinceNow, orders) =>
-      (monthsSinceNow, orders.size)
+    val c: Map[MonthsSinceNow, Int] = b.map {
+      case (monthsSinceNow, orders) => (monthsSinceNow, orders.size)
     }
 
-    //    c.toSeq.sortBy {
-    //      case (monthsSinceNow, orders) => monthsSinceNow.toString
-    //    }
+    /*        c.toSeq.sortBy {
+              case (monthsSinceNow, orders) => monthsSinceNow.toString
+            }*/
+
     c.map(obj => {
       obj._1 match {
         case LessThanOneMonth => ("<1: " + obj._2)
